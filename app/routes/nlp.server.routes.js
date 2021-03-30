9afbd217281a80d297526cabab419d68645c2769
buzhaobin <buzhaobin@gmail.com>
2021-03-29 21:56:27 -0400

    First commit.

---
+++ app/routes/nlp.server.routes.js
@@ -0,0 +1,11 @@
+module.exports = function (app) {
+  var nlp = require("../controllers/nlp.server.controller");
+
+  app.route("/").get(nlp.renderURLForm);
+
+  app
+    .route("/processNLP")
+    .get(nlp.scrapeURL, nlp.processText, nlp.renderResult);
+
+  app.route("/api/run").get(nlp.run, nlp.processText, nlp.renderResultJson);
+};
